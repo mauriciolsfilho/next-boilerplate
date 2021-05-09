e6bcd33e6788f0abb24add801c59f2ae41ec7801
Mauricio Linhares <mauriciolsfilho75@gmail.com>
2021-05-09 19:51:36 -0300

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const bla = 'var'
+  const seila = 'variavel'
   return (
     <div className={styles.container}>
       <Head>

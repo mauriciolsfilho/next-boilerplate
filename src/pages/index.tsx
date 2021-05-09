f1a546e0f88f2b0c7ce539fddf5b203ee9867695
Mauricio Linhares <mauriciolsfilho75@gmail.com>
2021-05-09 20:19:14 -0300

    husky running

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  // const bla = 'var'
+  const bla = 'var'
   // const seila = 'variavel'
   return (
     <div className={styles.container}>
